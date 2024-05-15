9d7ef95753dd624783ff221f0a2e81d48c1c99f3
Sprou-t <150780574+Sprou-t@users.noreply.github.com>
2024-05-15 13:39:39 +0800

    incomplete board function

--- js-script.js
+++ js-script.js
@@ -1,42 +1,59 @@
 //create gameboard obj
 // store the gameboard in a 2d array inside of a gameboard Object
-let gameboard =( ()=>{
+let gameboard =()=>{
     let board = [ //initial board
         ['','',''],
         ['','',''],
         ['','','']
     ]
-
+ 
+ 
     //function to initialise the board
-
-    //function to update and mark the board
-
+    const initialiseBoard = () => board;
+   
+    //function to update the board
+ 
+ 
     //function to restart the game when restart button clicked
-
+ 
+ 
     //function to print out the board in the console for debugging purposes
-}) 
-
-//create a player obj
-let player = (name,marking) =>{//it is ok to include eventlistener as that is when we create the player from the factory function
-    
-    getName = () => name;
-    getMarking = () => marking;
-
+ }
+ 
+ 
+ //create 2 player objs using factory function(can be reused)
+ let player = (name,marking) =>{//it is ok to include eventlistener as that is when we create the player from the factory function
+   
+    const getName = () => name;
+    const getMarking = () => marking;
+ 
+ 
     return {getName,getMarking}
-}
-
-player();
-//create a gamecontroller object to control the game using all the conditionals!
-let gameController = () =>{
+ }
+ 
+ 
+ player();
+ //create a gamecontroller object to control the game using all the conditionals!
+ let gameController = (() =>{
     //create a function that designates the users and changes the users in every turn using conditionals
         //user marks the board
-
-    //restarts the game
-
-    //creates a checkwin function that checks all the win conditions. Hardcode the win conditions
-    let checkwin = () =>{
-
-    }
-}
+ 
+ 
+   
+ 
+ 
+   
+ })()
+ 
+ 
+ //creates a checkwin function that checks all the win conditions. Hardcode the win conditions
+ let checkwin = () =>{
+ 
+ 
+ }
+ 
+ 
+ //restarts the game
+ 
 
 
